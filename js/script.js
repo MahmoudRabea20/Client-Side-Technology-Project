2292f90abd1af12a6e5a3c5beebbff9ed0d59383
MahmoudRabea20 <mahmoudrabea1336@gmail.com>
2025-04-14 17:38:20 +0200

    Creating the dashboard page of admin

--- js/script.js
+++ js/script.js
@@ -1,38 +1,25 @@
+
 /*       Get data of categories from firebase */
 
-async function getCategories() {
+function getCategories(){
+    let selectInput = document.getElementById("catm");
+    selectInput.innerHTML = `<option selected disabled>Select The Category of This Product</option>`;
     
-    try{
-        // let res = await fetch("https://client-side-technology-default-rtdb.firebaseio.com/Categories.json");
-        // let categories = await res.json();
 
-        let categories = {
-            key1:{
-                id: 1,
-                name: "cat1"
-            },
-            key2:{
-                id: 1,
-                name: "cat2"
-            },
-            key3:{
-                id: 1,
-                name: "cat3"
-            }
-        }
-
-        let selectInput = document.getElementById("catm");
-        selectInput.innerHTML = `<option selected disabled>Select The Category of This Product</option>`;
+    fetch("https://client-side-technology-default-rtdb.firebaseio.com/category.json")
+    .then((res)=>{
+        return res.json();
+    }).then((categories)=>{
+        console.log(categories);
+        
 
         for (const key in categories) {
             selectInput.innerHTML += `<option>${categories[key].name}</option>`;
         }
-    }
-    catch(error){
-        console.log(error);
-    }
-
-    
+    }).catch((err)=>{
+        console.log(err);
+        
+    })
 }
 
 getCategories();
@@ -42,11 +29,11 @@ getCategories();
 const contentData = {
     section1: 
         `
-                        <div id="my-formm" class="row my-input d-flex justify-content-center pt-3">
+            <div id="my-formm" class="row my-input d-flex justify-content-center pt-3">
                 <div class="text-center">
                     <h1>Add Products</h1>
                 </div>
-                <div class="col-12 col-sm-10 col-lg-8  p-5 ">
+                <div class="col-12 col-sm-10 col-lg-8  p-3 ">
                     <form>
                         <div>
                             <input type="text " class="form-control" hidden id="keym" placeholder="Enter Key">
@@ -57,8 +44,8 @@ const contentData = {
                             <span id="idError" style="color: red; visibility: hidden;">Error</span>
                         </div>
                         <div class="mb-3">
-                            <label for="titlem" class="form-label">Title:</label>
-                            <input type="text" class="form-control" id="titlem" placeholder="Enter The Title">
+                            <label for="titlem" class="form-label">Name:</label>
+                            <input type="text" class="form-control" id="titlem" placeholder="Enter The Name">
                             <span id="titleError" style="color: red; visibility: hidden;">Error</span>
                         </div>
                         <div class="mb-3">
@@ -83,7 +70,12 @@ const contentData = {
                             <input type="text" class="form-control" id="imagem" placeholder="Enter The Source Of Image">
                             <span id="sourceError" style="color: red; visibility: hidden;">Error</span>
                         </div>
-                        <input type="submit" class="my-buttonm btn" value="Add Product"></input>
+                        <div class="mb-3">
+                            <label for="quantitym" class="form-label">Stock Quantity:</label>
+                            <input type="text" class="form-control" id="quantitym" placeholder="Enter The quantity of stock">
+                            <span id="quantityError" style="color: red; visibility: hidden;">Error</span>
+                        </div>
+                        <input type="submit" class="my-buttonm btn" value="Add New Product"></input>
                     </form>
 
                     
@@ -93,21 +85,33 @@ const contentData = {
             </div>
 
             
-            <div id="my-pro" class="row my-productsm  ">
+            <div id="my-pro" class="row my-productsm d-flex justify-content-center ">
                 <div  class="col-12">
                     <div class="text-center my-3">
                         <h1>All Produts</h1>
                     </div>
-                    <div><button class="my-buttonm my-5 btn" onclick="getProducts();">Show Products</button></div>
+                    <div><button class="my-buttonm my-3 btn" onclick="getProducts();">Show Products</button></div>
                 </div>
                 
             </div>
+
         
         `
     
     ,
-    section2: "This sections for categories",
-    section3: "This sections for orders",
+    
+    section3: "This sections for Categories",
+    section3: `
+        <div id="my-ordersm" class="row my-input d-flex justify-content-center pt-3">
+            <div  class="col-12">
+                <div class="text-center my-3">
+                    <h1>Orders</h1>
+                </div>
+            </div>
+            
+        </div>
+    
+    `,
     section4: "Add Admins"
 };
 
@@ -129,6 +133,17 @@ tabLinks.forEach(link => {
         // Update content display
         const selectedContent = this.getAttribute('data-content');
         contentDisplay.innerHTML = contentData[selectedContent];
+        if(selectedContent === "section1"){
+            
+            setTimeout(()=>{
+                getCategories();
+            },100);
+        }
+        if(selectedContent === "section3"){
+            setTimeout(()=>{
+                showOrders();
+            }, 100);
+        }
     });
 });
 
@@ -145,6 +160,7 @@ let categoryInput = document.getElementById("catm");
 let priceInput = document.getElementById("pricem");
 let sourceInput = document.getElementById("imagem");
 let keyInput = document.getElementById("keym");
+let quantityInput = document.getElementById("quantitym")
 let forms = document.getElementsByTagName("form");
 
 let idSpan = document.getElementById("idError");
@@ -153,6 +169,8 @@ let titleSpan = document.getElementById("titleError");
 let descriptionSpan = document.getElementById("descriptionError");
 let sourceSpan = document.getElementById("sourceError");
 let catSpan = document.getElementById("catError");
+let quantitySpan = document.getElementById("quantityError");
+
 
 let productForm = forms[0];
 
@@ -163,9 +181,60 @@ let isTitle = false;
 let isDes = false;
 let isPrice = false;
 let isSource = false;
+let isQuantity = false;
+
+quantityInput.addEventListener("input",function(e) {
+    let quantityValue = e.target.value;
+    let regExp = /^\d+$/ig;
+    
+    if(!regExp.test(quantityValue)){
+        isQuantity = false;
+        quantitySpan.innerHTML = "Quantity is not correct"; 
+        quantitySpan.style.visibility = "visible";
+    }
+    else {
+        isQuantity = true;
+        quantitySpan.innerHTML = "Error"; 
+        quantitySpan.style.visibility = "hidden";
+    }
+    
+})
 
 idInput.addEventListener("input",function(e) {
+
     let idValue = e.target.value;
+
+
+    fetch("https://client-side-technology-default-rtdb.firebaseio.com/product.json")
+    .then((res)=>{
+        return res.json();
+    }).then((res2)=>{
+        // console.log(res2);
+        
+
+        for (const key in res2) {
+
+            // console.log(res2[key]);
+
+            if(res2[key].ID === idValue ){
+
+
+                isID = false;
+                idSpan.innerHTML = "Id has already been found"; 
+                idSpan.style.visibility = "visible";
+
+            }
+        }
+    }).catch((err)=>{
+        console.log(err);
+        
+    })
+
+
+
+
+
+
     let regExp = /^\d+$/ig;
     
     if(!regExp.test(idValue)){
@@ -187,7 +256,7 @@ titleInput.addEventListener("input",function(e) {
 
     if(!regExp.test(titleValue)){
         isTitle = false;
-        titleSpan.innerHTML = "Title is not correct"; 
+        titleSpan.innerHTML = "Name is not correct"; 
         titleSpan.style.visibility = "visible";
     }
     else {
@@ -248,6 +317,11 @@ sourceInput.addEventListener("input",function(e) {
     
 })
 
+categoryInput.addEventListener("change",function() {
+    catSpan.style.visibility = "hidden";
+    
+})
+
 productForm.addEventListener("submit", function(e){
 
     let idValue = idInput.value;
@@ -256,20 +330,25 @@ productForm.addEventListener("submit", function(e){
     let descriptionValue = descriptionInput.value;
     let sourceValue = sourceInput.value;
     let catValue = categoryInput.value;
+    let quantityValue = quantityInput.value;
 
-    if(isDes === false || isID === false || isPrice === false || isSource === false || isTitle === false)
+    if(isDes === false || isID === false || isPrice === false || isSource === false || isTitle === false || isQuantity == false)
     {
+        if(catValue == "Select The Category of This Product"){
+            catSpan.innerHTML = "Select a category"; 
+            catSpan.style.visibility = "visible";
+        }
+        if(quantityValue === ""){
+            quantitySpan.innerHTML = "Enter a quantity"; 
+            quantitySpan.style.visibility = "visible";
+        }
         if(idValue === ""){
             idSpan.innerHTML = "Enter a id"; 
             idSpan.style.visibility = "visible";
         }
         if(titleValue === ""){
-            titleSpan.innerHTML = "Enter a title"; 
+            titleSpan.innerHTML = "Enter a name"; 
             titleSpan.style.visibility = "visible";
-        }
-        if(catValue == "Select The Category of This Product"){
-            catSpan.innerHTML = "Select a category"; 
-            catSpan.style.visibility = "visible";
         }
         if(priceValue === ""){
             priceSpan.innerHTML = "Enter a price"; 
@@ -292,7 +371,8 @@ productForm.addEventListener("submit", function(e){
             Price: priceValue,
             Description: descriptionValue,
             Category: catValue,
-            Source: sourceValue
+            Source: sourceValue,
+            Quantity: quantityValue
         }
         
         addProducts(product);
@@ -313,11 +393,13 @@ function clearFields(){
     descriptionInput.value = "";
     sourceInput.value = "";
     categoryInput.value = "Select The Category of This Product";
+    quantityInput.value = "";
 }
 
 function addProducts(product) {
     
-    // console.log(product);
+    console.log(product);
+    console.log("asyhdgfsjaygfsdj");
     
     let check = keyInput.value ;
     if (!check) {
@@ -336,6 +418,8 @@ function addProducts(product) {
         })
         
     } else {
+        console.log("PAAAAAAAAAAAATTTTTTTTTTTTTCCCCCCCCHHHHHHhhh");
+        
         
         fetch(`https://client-side-technology-default-rtdb.firebaseio.com/product/${check}.json`,{
             method: 'PATCH',
@@ -369,7 +453,7 @@ function getProducts(){
             <div class="text-center my-3">
                 <h1>All Produts</h1>
             </div>
-            <div><button class="my-buttonm my-5 btn" onclick="getProducts();">Show Products</button></div>
+            <div><button class="my-buttonm my-3 btn" onclick="getProducts();">Show Products</button></div>
         </div>
     
     `;
@@ -387,8 +471,8 @@ function getProducts(){
         for (const key in res2) {
             // console.log(res2[key]);
             productsDiv.innerHTML += `
-            <div class="col-lg-4 col-xl-3 col-12 col-sm-6 my-5 d-flex justify-content-center">
-                <div class="card" style="width: 15rem;">
+            <div class="col-lg-4 col-xl-3 col-12 col-sm-6 my-3 d-flex justify-content-center">
+                <div class="card" style="width: 20rem;">
                     <img src="../Resources/${res2[key].Source}.png" 
                         class="card-img-top img-fluid" 
                         alt="${res2[key].Title}" 
@@ -396,9 +480,9 @@ function getProducts(){
                     <div class="card-body">
                         <h5 class="card-title m-auto">${res2[key].Title}</h5>
                         <h5 class="card-title m-auto">${res2[key].Price} E£</h5>
-                        <p class="card-text">${res2[key].Description}</p> 
+                        <h5 class="card-title m-auto">Stock Quantity: ${res2[key].Quantity}</h5>
                         <a class="btn my-buttonm" onclick="removeProduct('${key}');">Remove Product</a>
-                        <a class="btn my-buttonm" href="#my-formm" onclick="updateProduct('${key}', '${res2[key].ID}', '${res2[key].Title}', '${res2[key].Description}', '${res2[key].Category}', '${res2[key].Price}','${res2[key].Source}');">Update Product</a>
+                        <a class="btn my-buttonm" href="#my-formm" onclick="updateProduct('${key}', '${res2[key].ID}', '${res2[key].Title}', '${res2[key].Description}', '${res2[key].Category}', '${res2[key].Price}','${res2[key].Source}','${res2[key].Quantity}')">Update Product</a>
                     </div>                                                                                                                                                                                                                   
                 </div>
             </div>
@@ -429,15 +513,15 @@ function removeProduct(id){
             console.log(err);
             
         })
+        setTimeout(getProducts,500);
     }
 
-    setTimeout(getProducts,500);
 
 
 }
 
 
-function updateProduct(ID,id, title, description, category, price, source) {
+function updateProduct(ID,id, title, description, category, price, source, quantity)  {
 
     
     keyInput.value = ID;
@@ -447,45 +531,112 @@ function updateProduct(ID,id, title, description, category, price, source) {
     descriptionInput.value = description;
     sourceInput.value = source;
     categoryInput.value = category;
+    quantityInput.value = quantity;
+    
 }
 
 
 
 
+/* Start of Orders of the Users */
+
+// function orderSection(){
+
+// }
 
 
+let res3 ;
+
+function showOrders(){
+    let ordersDiv = document.getElementById("my-ordersm");
 
 
+    ordersDiv.innerHTML = "";
 
-
-
-// b.addEventListener("submit", function(e) {
-//     let id = document.getElementById("idm");
-//     let value1 = id.value;
-//     let title = document.getElementById("titlem");
-//     let value2 = title.value;
-//     let description = document.getElementById("desm");
-//     let value3 = description.value;
-//     let category = document.getElementById("catm");
-//     let value4 = category.value;
-//     let price = document.getElementById("pricem");
-//     let value5 = price.value;
-//     let source = document.getElementById("imagem");
-//     let value6 = source.value;
-
-//     if(value1 == "12"){
-//         e.preventDefault();
-//     }
-//         console.log(value1);
+    ordersDiv.innerHTML =
+    `
+        <div id="my-ordersm" class="row my-input d-flex justify-content-center pt-3">
+            <div  class="col-12">
+                <div class="text-center my-1">
+                    <h1>Orders</h1>
+                </div>
+            </div>
+            
+        </div>
     
+    `
+
+
+    fetch("https://client-side-technology-default-rtdb.firebaseio.com/product.json")
+    .then((res)=>{
+        return res.json();
+    }).then((res2)=>{
+        // console.log(res2);
+        
+
+        for (const key in res2) {
+            // console.log(res2[key]);
+            ordersDiv.innerHTML += `
+            <div class="col-lg-4 col-xl-3 col-12 col-sm-6 my-3 d-flex justify-content-center">
+                <div class="card" style="width: 15rem;">
+                    <img src="../Resources/${res2[key].Source}.png" 
+                        class="card-img-top img-fluid" 
+                        alt="${res2[key].Title}" 
+                        style=" height: 250px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title m-auto">${res2[key].Title}</h5>
+                        <h5 class="card-title m-auto">${res2[key].Price} E£</h5>
+                        <a class="btn my-buttonm" onclick="rejectOrder('${key}','${res2}','${res2[key].ID}');">Reject The Order</a>
+                        <a class="btn my-buttonm" onclick="rejectOrder('${key}','${res2}','${res2[key].ID}');">Confirm The Order</a>
+                    </div>                                                                                                                                                                                                                   
+                </div>
+            </div>
+            `;
+
+            console.log(key);
+            
+        
+
+            
+        }
+    }).catch((err)=>{
+        console.log(err);
+        
+    })
+}
+
+
+function rejectOrder(id,ob1,ob2){
+
+
+    console.log("HOOOOOOOOOOO");
     
+    console.log(id);
+    console.log(ob1);
+    console.log(ob2);
 
-// } )
+    
+    let isConfirm = confirm("Do you want to reject this order?");
 
-/** End Form of Products */
+    if(isConfirm){
+        fetch(`https://client-side-technology-default-rtdb.firebaseio.com/product/${id}.json`,{
+            
+            method: 'DELETE'
+        }).then((res2)=>{
+            console.log(res2);
+        }).catch((err)=>{
+            console.log(err);
+            
+        })
+        setTimeout(showOrders,500);
+    }
 
-// let d = fetch("https://fakestoreapi.com/products")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
+
+
+}
+
+
+
+
 
 
